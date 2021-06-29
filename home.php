a25a0dc41d2184a4f4471bcf8272dfc90a87f68c
parice esperou <pesperou@gmail.com>
2021-06-29 10:24:16 +0200

    creation du fichier home.php

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Mon blog</title>
+</head>
+
+<body>
+    <div>
+        <h1>Patrice Esperou ' Blog</h1>
+        <p>En construction</p>
+    </div>
+</body>
+</html>
